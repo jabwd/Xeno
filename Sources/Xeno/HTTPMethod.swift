fd053a3e881037d24f48a29b2ea7e38ec198e2bb
Antwan van Houdt <jabwd@exurion.com>
2018-03-20 00:51:33 +0100

    Added HTTPMethod, HTTPStatusCode

---
+++ Sources/Xeno/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  XenoPackageDescription
+//
+//  Created by Antwan van Houdt on 20/03/2018.
+//
+
+public enum HTTPMethod: String {
+	case get    = "GET"
+	case put    = "PUT"
+	case post   = "POST"
+	case patch  = "PATCH"
+}
--- Sources/Xeno/HTTPRequest.swift
+++ Sources/Xeno/HTTPRequest.swift
@@ -5,11 +5,16 @@
 //  Created by Antwan van Houdt on 19/03/2018.
 //
 
-class HTTPParser {
-    
+class HTTPRequest {
+	
+	let method: HTTPMethod
     var headers: [String: String] = [:]
+	let body: String
     
     init(_ request: String) {
-        
+		let lines = request.split(separator: "\r\n")
+		for line in lines {
+			
+		}
     }
 }
