8557415cc4447b81225879af3e582e8cc796af2e
Antwan van Houdt <jabwd@exurion.com>
2018-03-20 13:01:18 +0100

    HTTP Request stuff

--- Sources/Xeno/HTTPRequest.swift
+++ Sources/Xeno/HTTPRequest.swift
@@ -5,16 +5,60 @@
 //  Created by Antwan van Houdt on 19/03/2018.
 //
 
-class HTTPRequest {
+struct HTTPRequest {
 	
 	let method: HTTPMethod
-    var headers: [String: String] = [:]
+    let path: String
+    let headers: [String: String]
 	let body: String
     
-    init(_ request: String) {
-		let lines = request.split(separator: "\r\n")
-		for line in lines {
-			
-		}
+    init?(request: String) {
+        let requestParts = request.split(separator: ":")
+        guard requestParts.count > 2 else {
+            return nil
+        }
+        
+        self.init(method: .get, path: "" , headers: [:], body: "")
     }
+    
+    init(method: HTTPMethod, path: String, headers: [String: String], body: String) {
+        self.method = method
+        self.path = path
+        self.headers = headers
+        self.body = body
+    }
+    
+    private static func scanHTTPMethodAndPath(_ line: Substring) -> (method: HTTPMethod, path: String)? {
+        let parts = line.split(separator: " ")
+        guard parts.count > 2 else {
+            return nil
+        }
+        return nil
+    }
+    
+   /* init(_ request: String) {
+        let requestParts = request.split(separator: "\r\n")
+        
+        
+        
+        
+        body = ""
+		let lines = request.split(separator: "\r\n")
+		for (index, line) in lines.enumerated() {
+            if line.isEmpty == true {
+                if (index+1) < lines.count {
+                    body = String(lines[index+1])
+                } else {
+                    body = ""
+                }
+                break
+            }
+            let parts = line.split(separator: ":")
+            guard parts.count > 1 else {
+                continue
+            }
+            headers[String(parts[0])] = String(parts[1])
+		}
+        method = .get
+    }*/
 }
