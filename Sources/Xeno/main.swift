9146de282e1b0b14ee34e8f77eae9ee490205005
Antwan van Houdt <jabwd@exurion.com>
2018-06-25 19:57:38 +0200

    More webservery like behavior

--- Sources/Xeno/main.swift
+++ Sources/Xeno/main.swift
@@ -8,8 +8,20 @@ let server = Xeno(host: "0.0.0.0", port: 8080, config: cfg)
 
 class FileRouter: Route {
     func handle(request: HTTPRequest) -> HTTPResponse {
-        let testPage = try! Data(contentsOf: URL(string: "file:///Users/jabwd/Desktop/Xeno.html")!)
-        let str = String(data: testPage, encoding: .utf8)!
+		let dir = cfg.sites[0].directoryRoot
+		var path = request.path
+		if path == "/" {
+			path = "/index.html"
+		}
+		// This path thing is unsafe, should not ever work like this :'D
+		guard let fileURL = URL(string: "file://\(dir)\(path)") else {
+			return HTTPResponse(status: .notFound, headers: [:], body: "Not found")
+		}
+		print("Request URL: \(fileURL)")
+		guard let data = try? Data(contentsOf: fileURL) else {
+			return HTTPResponse(status: .notFound, headers: [:], body: "File not found")
+		}
+        let str = String(data: data, encoding: .utf8) ?? ""
         return HTTPResponse(status: .ok, headers: [:], body: str)
     }
 }
